e7a9bdf632b1ac55ed55040403d174901da5abc0
Kim Skovhus Andersen <kimskovhusandersen@gmail.com>
2021-07-13 19:41:38 +0200

    test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,13 +1,14 @@
-import '@testing-library/jest-dom';
+/**
+ * @jest-environment jsdom
+ */
 import { render } from '@testing-library/react';
 import { expect } from 'chai';
 import * as React from 'react';
 import App from './App';
-
 describe('<App>', () => {
   it('renders header', () => {
     const { getByText } = render(<App />);
-    const headerElement = getByText(/React + TypeScript + Twind/i);
-    expect(headerElement).toBeInTheDocument();
+    const headerElement = getByText(/Twind react typescript/i);
+    expect(document.body.contains(headerElement));
   });
 });
