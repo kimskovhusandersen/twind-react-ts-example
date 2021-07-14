1dbfb2005dd8490b0a111b2fd81c271451ab04b4
Kim Skovhus Andersen <kimskovhusandersen@gmail.com>
2021-07-14 09:36:23 +0200

    use styled from @twind/react

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,43 +1,28 @@
+import { styled } from '@twind/react';
 import * as React from 'react';
-import { tw, apply } from 'twind';
-import type { InlineDirectiveMap } from '../types';
-import { lazy } from '../utils';
 
-export interface AvatarProps
-  extends Omit<React.ImgHTMLAttributes<HTMLImageElement>, 'size'> {
-  /**
-   * Instance-level classNames will override local classNames
-   */
-  className?: string;
-  /**
-   * Determines if the avatar will be round
-   */
-  round?: boolean;
-  /**
-   * The size of the avatar.
-   */
-  size?: 'sm' | 'md' | 'lg';
-  /**
-   * The URL of the avatar image
-   */
-  src?: string;
-}
+const StyledAvatar = styled('img', {
+  base: `rounded-md`,
+  variants: {
+    size: {
+      sm: `h-12 w-12`,
+      md: `h-16 w-16`,
+      lg: `h-20 w-20`,
+    },
+    round: {
+      true: `rounded-full`,
+    },
+  },
+  defaults: {
+    size: 'md',
+  },
+});
 
-type Size = 'sm' | 'md' | 'lg';
-
-const sizeMap: InlineDirectiveMap<Size> = {
-  sm: lazy`h-12 w-12`,
-  md: lazy`h-16 w-16`,
-  lg: lazy`h-20 w-20`,
-};
-
-export const Avatar = (props: AvatarProps) => {
-  const { size = 'md', alt, round, className, ...rest } = props;
-  const appliedClassNames = apply([
-    sizeMap[size],
-    `rounded-${round ? 'full' : 'md'}`,
-  ]);
-  return (
-    <img alt={alt} className={tw(appliedClassNames, className)} {...rest} />
-  );
+export const Avatar = (props: {
+  [x: string]: any;
+  src: string;
+  alt?: string;
+}) => {
+  const { alt, src, ...rest } = props;
+  return <StyledAvatar src={src} alt={alt || ''} {...rest} />;
 };
