e68e6d1a2775f89280f7c65b75275a9400197215
Kim Skovhus Andersen <kimskovhusandersen@gmail.com>
2021-07-14 10:56:12 +0200

    fix typing and storybook

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,5 +1,46 @@
+import type { StyledComponent } from '@twind/react';
 import { styled } from '@twind/react';
 import * as React from 'react';
+import type { BaseComponent } from '../types';
+export interface AvatarProps
+  extends BaseComponent,
+    Omit<
+      StyledComponent<
+        {
+          size: {
+            sm: string;
+            md: string;
+            lg: string;
+          };
+          round: {
+            true: string;
+          };
+        },
+        'img'
+      >,
+      'className' | 'selector'
+    >,
+    Omit<
+      React.HTMLAttributes<HTMLImageElement>,
+      'children' | 'className' | 'css'
+    > {
+  /**
+   * Determines if the button is fully rounded
+   */
+  round?: boolean;
+  /**
+   * Controls the text size and padding of the button
+   */
+  size?: 'sm' | 'md' | 'lg';
+  /**
+   * Determines the url to the image
+   */
+  src: string;
+  /**
+   * The alt of the image
+   */
+  alt?: string;
+}
 
 const StyledAvatar = styled('img', {
   base: `rounded-md`,
@@ -18,11 +59,18 @@ const StyledAvatar = styled('img', {
   },
 });
 
-export const Avatar = (props: {
-  [x: string]: any;
-  src: string;
-  alt?: string;
-}) => {
-  const { alt, src, ...rest } = props;
-  return <StyledAvatar src={src} alt={alt || ''} {...rest} />;
+export const Avatar = React.forwardRef<HTMLImageElement, AvatarProps>(
+  (props) => {
+    const { alt, src, className, ...rest } = props;
+    return (
+      <StyledAvatar src={src} alt={alt || ''} className={className} {...rest} />
+    );
+  },
+);
+
+// eslint-disable-next-line functional/immutable-data
+Avatar.defaultProps = {
+  className: '',
+  alt: '',
+  src: undefined,
 };
